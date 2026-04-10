ded022acf3205297d7a568c38e47a6970ceea105
student <student@stanowisko.sala308>
2026-04-10 13:40:45 +0200

    Zrobiono zadanie 1

---
+++ zadanie1/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {    
+        ArrayList<String> studenci = new ArrayList<String>();
+        studenci.add("Mateusz");
+        studenci.add("Kamil");
+        studenci.add("Nikodem");
+        System.out.println("Lista studentow" + studenci);
+    }
+}
